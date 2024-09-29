79991402ca6c6b15d28876d6e6595205163b2b08
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2024-09-30 01:50:13 +0800

    image path

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, '../src/components/images')));
+app.use('/images', express.static(path.join(__dirname, '/components/images')));
 
 // Setup multer for image uploads
 const storage = multer.diskStorage({
