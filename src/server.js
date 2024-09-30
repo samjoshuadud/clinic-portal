b508718e88ba0edbe693ee3642fb129f3afc98a8
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2024-09-30 15:30:35 +0800

    what the hell

--- src/server.js
+++ src/server.js
@@ -11,7 +11,9 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({origin: url}));
+// app.use(cors({origin: url})); // production
+
+app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
